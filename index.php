c589431d626620c2bfda60ec3b700370023bd953
geoffreyFRANZ <franz.geoffrey@hotmail.fr>
2026-01-04 16:03:33 +0100

    change title

--- index.php
+++ index.php
@@ -55,7 +55,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Cosmic Background</title>
+    <title>Geoffrey Franz</title>
     <link rel="stylesheet" href="newstyle.css">
 </head>
 
